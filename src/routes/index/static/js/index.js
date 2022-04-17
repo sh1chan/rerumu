7b16bb762fe8d56c376bf33268253c7c2cb7aa75
Nodiru Gaji <c0d3r.nodiru.gaji@gmail.com>
2022-04-17 20:35:00 +0300

    echo message throw WebSocket

--- src/routes/index/static/js/index.js
+++ src/routes/index/static/js/index.js
@@ -1,6 +1,32 @@
 // TODO: BackendURI -> BackendEndpoint
 
 
+class ListenWebsocketMessages {
+
+  constructor(event) {
+    this.event = event;
+  }
+
+  run() {
+    console.log(this.event.data);
+  }
+
+}
+
+
+class EchoServerSocket {
+
+  constructor(socket) {
+    this.socket = socket;
+  }
+
+  run() {
+    this.socket.send("Hello, World!");
+  }
+
+}
+
+
 class InitDataReceiver {
 
   constructor(endpoint) {
@@ -8,19 +34,27 @@ class InitDataReceiver {
   }
 
   run() {
+    // TODO: ignore or process the event
     let socket = new WebSocket("ws://127.0.0.1:12345");
+
     socket.onopen = function(event) {
-      console.log("OnOpen" + event);
+      let ess = new EchoServerSocket(socket);
+      ess.run();
     }
+
     socket.onmessage = function(event) {
-      console.log("OnMessage" + event);
+      let lwm = new ListenWebsocketMessages(event);
+      lwm.run();
     }
+
     socket.onclose = function(event) {
       console.log("OnClose" + event);
     }
+
     socket.onerror = function(event) {
       console.log("OnError" + event);
     }
+
   }
 
 }
